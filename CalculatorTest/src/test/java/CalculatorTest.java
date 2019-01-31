8e19bff82238e128bcbed5f20e6dea1cf93dde97
ADDQ <ADDQ@172.16.19.80>
2019-01-31 10:49:32 +0100

    7 commit

--- CalculatorTest/src/test/java/CalculatorTest.java
+++ CalculatorTest/src/test/java/CalculatorTest.java
@@ -70,11 +70,11 @@ public class CalculatorTest {
     @Test
     public void Addition()
     {
-        CalculatorSession.findElementByName("Five").click();
+        CalculatorSession.findElementByName("Seven").click();
         CalculatorSession.findElementByName("Plus").click();
         CalculatorSession.findElementByName("Two").click();
         CalculatorSession.findElementByName("Equals").click();
-        Assert.assertEquals("7", _GetCalculatorResultText());
+        Assert.assertEquals("9", _GetCalculatorResultText());
     }
 
     @Test

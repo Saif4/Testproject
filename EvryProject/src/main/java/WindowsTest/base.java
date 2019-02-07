b87de749445f6bc0df5b08b1b2e8544d57a90b66
ADDQ <ADDQ@LAPTOP-J0378V4D>
2019-02-07 21:11:41 +0100

    test

--- EvryProject/src/main/java/WindowsTest/base.java
+++ EvryProject/src/main/java/WindowsTest/base.java
@@ -2,8 +2,13 @@ package WindowsTest;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Set;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.util.Assert;
 
 import io.appium.java_client.windows.WindowsDriver;
 
@@ -27,11 +32,20 @@ public class base {
 			//cap.setCapability("appArguments", "MyTestFile.txt");
 			//cap.setCapability("appWorkingDir", "C:\\MyTestFolder\\");
 			driver = new WindowsDriver(new URL("http://127.0.0.1:4723"), cap);
+			//Actions a = new Actions(driver);
+			//a.moveToElement(driver.findElementByName("Calculator")).build().perform();
+			//WebElement newWindow = driver.findElementByName("Calculator");
+			//String newWindowHandle = newWindow.getAttribute("NativeWindowHandle");
+			//driver.switchTo().window(newWindowHandle);
+			//WebElement ChildWindow = driver.findElementByClassName("Windows.UI.Core.CoreWindow");
+			driver.findElementByName("Text Editor").sendKeys("hejjj");
+			String te = driver.findElementByAccessibilityId("CalculatorResults").getText();
+		    driver.getWindowHandle();
+		    driver.switchTo(te);
+
 			
-			
-			}
 		
-	
+		}
 	
 
 
